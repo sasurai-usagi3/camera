db7705b74b42f96b71426623a723de796a4d9155
宇山拓夢 <sasurai.usagi3@gmail.com>
2016-11-30 14:58:49 +0900

    テスト

--- docs/js/application.js
+++ docs/js/application.js
@@ -3,8 +3,16 @@ window.addEventListener("load", () => {
   deviceNavigator.then((s) => {
     let target = document.getElementById("target");
     let ctracker = new clm.tracker();
+    let result = document.getElementById("result");
+    let context = result.getContext("2d");
+    let update = () => {
+      requestAnimationFrame(update);
+      context.clearRect(0, 0, result.width, result.height);
+      ctracker.draw(result);
+    };
     target.src = window.URL.createObjectURL(s);
     ctracker.init(pModel);
     ctracker.start(target);
+    update();
   });
 });

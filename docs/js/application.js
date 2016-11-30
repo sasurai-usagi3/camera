cbe587fd8b750fc65e429f7870d8d363fe7f1ed4
宇山拓夢 <sasurai.usagi3@gmail.com>
2016-11-30 14:27:45 +0900

    顔認識のライブラリ使用

--- docs/js/application.js
+++ docs/js/application.js
@@ -2,6 +2,9 @@ window.addEventListener("load", () => {
   let deviceNavigator = navigator.mediaDevices.getUserMedia({ audio: false, video: true });
   deviceNavigator.then((s) => {
     let target = document.getElementById("target");
+    let ctracker = new clm.tracker();
     target.src = window.URL.createObjectURL(s);
+    ctracker.init(pModel);
+    ctracker.start(target);
   });
 });
